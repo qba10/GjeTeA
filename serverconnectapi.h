2711370d338b703d49939d63c97354add5e203fa
qba10 <qba100@gmail.com>
2014-05-26 17:21:24 +0200

    dodanie plikow server connect

---
+++ serverconnectapi.h
@@ -0,0 +1,17 @@
+#ifndef SERVERCONNECTAPI_H
+#define SERVERCONNECTAPI_H
+
+#include "Loader.h"
+namespace SSJ{
+    class ServerConnectAPI
+    {
+    protected:
+          static  sf::TcpSocket* Socket;
+            sf::Mutex mutex;
+    public:
+
+        void ServerListener();
+        static void SendToServer(string);
+    };
+}
+#endif // SERVERCONNECTAPI_H
